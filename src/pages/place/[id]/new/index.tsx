5961dac5664aa92879b6c420ae867e950288d6db
tkjo155 <takajo.sum15@gmail.com>
2024-04-23 11:22:40 +0900

    DatePicker/導入

--- src/pages/place/[id]/new/index.tsx
+++ src/pages/place/[id]/new/index.tsx
@@ -5,7 +5,10 @@ import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import { InputFishLog } from '@/Types'
 import { CREATE_FISHLOG } from '@/graphql/createFishlog'
 import { GET_FISHLOG } from '@/graphql/getFishlog'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import { DatePicker } from "@nextui-org/date-picker";
+import { format } from 'date-fns'
+import { useState } from 'react'
 
 const FishLogForm = () => {
   const router = useRouter();
@@ -15,13 +18,20 @@ const FishLogForm = () => {
 
   const { register, handleSubmit, clearErrors, watch, setValue, control, formState: { errors } } = useForm<InputFishLog>();
 
+    // 選択された日付を管理するstate
+    const [selectedDate, setSelectedDate] = useState("");
+    // 日付が変更されたときのハンドラ
+    const handleDateChange = (date:any) => {
+      setSelectedDate(date);
+    };
+
   const onSubmit: SubmitHandler<InputFishLog> = async (formData) => {
     try {
       await createFishlog({
         variables: {
           create: {
             placeId: Number(id),
-            date: formData.date,
+            date: String(selectedDate),
             image: "",
             fishName: formData.fishName,
             isSunny: formData.weather.includes('sunny'),
@@ -53,18 +63,11 @@ const FishLogForm = () => {
       <h1 className='text-center text-2xl mb-4'>釣行記録登録</h1>
       <div className='mb-4'>
         <label className='text-lg'>日にち</label>
-        {errors.date && <span className='text-red-500 ml-2'>{errors.date.message}</span>}
-        <Input
-          {...register('date', {
-            required: '日にちは必須です',
-            maxLength: {
-              value: 15,
-              message: '日にちは15文字以内で入力してください',
-            },
-          })}
-          type='text'
-          className='w-full mt-2'
-        />
+        <DatePicker
+          label="Fishing date"
+          className="max-w-[284px]"
+          onChange={handleDateChange}
+          />
       </div>
       <div className='mb-4'>
         <label className='text-lg'>魚の名前</label>
