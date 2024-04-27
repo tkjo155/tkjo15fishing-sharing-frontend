c0fda6802afe55393900917db3d21f6597728b61
tkjo155 <takajo.sum15@gmail.com>
2024-04-27 18:40:04 +0900

    form/UI変更

--- src/pages/place/[id]/new/index.tsx
+++ src/pages/place/[id]/new/index.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from '@apollo/client'
-import { Button, Checkbox, CheckboxGroup, Input, Navbar, NavbarBrand, Radio, RadioGroup } from '@nextui-org/react'
+import { Button, Checkbox, CheckboxGroup, Input, Navbar, NavbarBrand, Radio, RadioGroup, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react'
 import Link from 'next/link'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import { InputFishLog } from '@/Types'
@@ -42,112 +42,143 @@ const FishLogForm = () => {
   };
 
   return (
-    <form className='mx-auto max-w-lg mt-8'>
-      <header className='bg-gray-900 py-4 mb-8 rounded-t-lg'>
-        <div className='container mx-auto'>
+    <div>
+      <header className='bg-gray-900 py-4 mb-8 w-full'>
+        <div className='container'>
           <Navbar style={{ backgroundColor: 'transparent' }}>
             <NavbarBrand>
-              <h1 className='text-white text-4xl font-bold px-6'>Fishing Spots</h1>
+              <h1 className='text-white text-3xl font-bold'>Fishing Spots</h1>
             </NavbarBrand>
           </Navbar>
         </div>
       </header>
-      <h1 className='text-center text-2xl mb-4'>釣行記録登録</h1>
-      <div className='mb-4'>
-        <label className='text-lg'>日にち</label>
-        {errors.date && <span className='text-red-500 ml-2'>{errors.date.message}</span>}
-        <DatePicker
-          {...register('date', {
-            required: '日にちは必須です'
-          })}
-          label="Fishing date"
-          className="max-w-[280px]"
-          onChange={(selectDate) => {
-            const date = `${selectDate.year}/${selectDate.month}/${selectDate.day}`;
-            setValue('date', date)
-            clearErrors('date');
-          }}
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='text-lg'>魚の名前</label>
-        {errors.fishName && <span className='text-red-500 ml-2'>{errors.fishName.message}</span>}
-        <Input
-          {...register('fishName', {
-            required: '魚名は必須です',
-            maxLength: {
-              value: 20,
-              message: '魚名は20文字以内で入力してください',
-            },
-          })}
-          type='text'
-          className='w-full mt-2'
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='text-lg'>天気</label>
-        {errors.weather && <span className='text-red-500 ml-2'>少なくとも1つの天気を選択してください</span>}
-      </div>
-      <div className='mb-4'>
-        <Controller
-          name='weather'
-          control={control}
-          render={({ field }) => (
-            <CheckboxGroup {...field}>
-              <Checkbox value='sunny'>晴れ</Checkbox>
-              <Checkbox value='rainy'>雨</Checkbox>
-              <Checkbox value='cloudy'>曇り</Checkbox>
-            </CheckboxGroup>
-          )}
-          rules={{ required: true }}
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='text-lg'>大きさ(cm)</label>
-        {errors.size && <span className='text-red-500 ml-2'>{errors.size.message}</span>}
-        <Input
-          {...register('size', {
-            required: '大きさは必須です',
-            max: {
-              value: 100,
-              message: '大きさは100㎝以内で入力してください',
-            },
-          })}
-          type='number'
-          className='w-full mt-2'
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='text-lg'>潮汐情報</label>
-        {errors.tide && <span className='text-red-500 ml-2'>{errors.tide.message}</span>}
-        <RadioGroup
-          value={watch('tide')}
-          name='tide'
-          className='flex flex-col mt-2'
-          onChange={(e) => {
-            const selectedValue = e.target.value;
-            setValue('tide', selectedValue);
-            clearErrors('tide');
-          }}
-        >
-          <Radio value='大潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2'>大潮</Radio>
-          <Radio value='中潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2'>中潮</Radio>
-          <Radio value='小潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2'>小潮</Radio>
-          <Radio value='長潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2'>長潮</Radio>
-          <Radio value='若潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2'>若潮</Radio>
-        </RadioGroup>
-      </div>
-      <div className='text-center'>
-        <Link href={`/place/${id}`} passHref>
-          <Button color='default' variant='shadow' size='lg' className='mr-4 mt-8'>
-            キャンセル
+      <form className='container max-w-3lg mx-auto'>
+        <h1 className='text-center text-4xl font-bold mb-3'>Fishing Record</h1>
+        <h2 className='text-center text-mx mb-20'>釣果データを入力してください</h2>
+        <Table className='shadow-lg mb-10' hideHeader removeWrapper aria-label='Example static collection table'>
+          <TableHeader>
+            <TableColumn className='border-r'>項目</TableColumn>
+            <TableColumn>情報</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className='border-t-5 border-gray-300 border-b bg-gray-100 font-semibold w-1/4'>
+                <label className='text-lg w-28'>日にち</label>
+                {errors.date && <span className='text-red-500 ml-2'>{errors.date.message}</span>}
+              </TableCell>
+              <TableCell className='border-t-5 border-b border-gray-300 w-3/4'>
+                <DatePicker
+                  {...register('date', {
+                    required: '日にちは必須です'
+                  })}
+                  label="Fishing date"
+                  className='w-full mt-2'
+                  onChange={(selectDate) => {
+                    const date = `${selectDate.year}/${selectDate.month}/${selectDate.day}`;
+                    setValue('date', date)
+                    clearErrors('date');
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='border-t border-gray-300 border-b bg-gray-100 font-semibold w-1/4'>
+                <label className='text-lg w-28'>魚の名前</label>
+                {errors.fishName && <span className='text-red-500 ml-2'>{errors.fishName.message}</span>}
+              </TableCell>
+              <TableCell className='border-t border-b border-gray-300 w-3/4'>
+                <Input
+                  {...register('fishName', {
+                    required: '魚名は必須です',
+                    maxLength: {
+                      value: 20,
+                      message: '魚名は20文字以内で入力してください',
+                    },
+                  })}
+                  type='text'
+                  label="Fish name"
+                  className='w-full mt-2'
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='border-t border-gray-300 border-b bg-gray-100 font-semibold w-1/4'>
+                <label className='text-lg w-28'>天気 <span className="text-red-500 text-sm">(複数選択可)</span></label>
+                {errors.weather && <span className='text-red-500 ml-2'>天気は必須です</span>}
+              </TableCell>
+              <TableCell className='border-t border-b border-gray-300 w-3/4'>
+                <Controller
+                  name='weather'
+                  control={control}
+                  render={({ field }) => (
+                    <CheckboxGroup {...field}>
+                      <Checkbox value='sunny' className='mb-2 ml-1'>晴れ</Checkbox>
+                      <Checkbox value='rainy' className='mb-2 ml-1'>雨</Checkbox>
+                      <Checkbox value='cloudy' className='mb-2 ml-1'>曇り</Checkbox>
+                    </CheckboxGroup>
+                  )}
+                  rules={{ required: true }}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='border-t border-gray-300 border-b bg-gray-100 font-semibold w-1/4'>
+                <label className='text-lg w-28'>大きさ<span className='text-red-500 text-sm'>(cm)</span></label>
+                {errors.size && <span className='text-red-500 ml-2'>{errors.size.message}</span>}
+              </TableCell>
+              <TableCell className='border-t border-b border-gray-300 w-3/4'>
+                <Input
+                  {...register('size', {
+                    required: '大きさは必須です',
+                    max: {
+                      value: 100,
+                      message: '大きさは100㎝以内で入力してください',
+                    },
+                  })}
+                  type='number'
+                  label="Fish size"
+                  className='w-full mt-2'
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='border-t border-b bg-gray-100 font-semibold w-1/4'>
+                <label className='text-lg w-28'>潮汐情報</label>
+                {errors.tide && <span className='text-red-500 ml-2'>{errors.tide.message}</span>}
+              </TableCell>
+              <TableCell className='border-t border-b border-gray-300 w-3/4'>
+                <RadioGroup
+                  value={watch('tide')}
+                  name='tide'
+                  className='flex flex-col mt-2'
+                  onChange={(e) => {
+                    const selectedValue = e.target.value;
+                    setValue('tide', selectedValue);
+                    clearErrors('tide');
+                  }}
+                >
+                  <Radio value='大潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2 ml-1'>大潮</Radio>
+                  <Radio value='中潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2 ml-1'>中潮</Radio>
+                  <Radio value='小潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2 ml-1'>小潮</Radio>
+                  <Radio value='長潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2 ml-1'>長潮</Radio>
+                  <Radio value='若潮' {...register('tide', { required: '潮汐情報は必須です' })} className='mb-2 ml-1'>若潮</Radio>
+                </RadioGroup>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <div className='text-center'>
+          <Link href={`/place/${id}`} passHref>
+            <Button color='default' variant='shadow' size='lg' className='mr-20 mt-15 mb-20'>
+              キャンセル
+            </Button>
+          </Link>
+          <Button color='primary' variant='shadow' size='lg' onClick={handleSubmit(onSubmit)}>
+            登録
           </Button>
-        </Link>
-        <Button color='primary' variant='shadow' size='lg' onClick={handleSubmit(onSubmit)}>
-          登録
-        </Button>
-      </div>
-    </form>
+        </div>
+      </form>
+    </div>
   )
 }
 
